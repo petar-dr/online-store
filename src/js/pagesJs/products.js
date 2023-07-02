72c4775fddba3560e830c9476c75f01ffa657476
Petar Dragojlovic <petardragojlovicbb@gmail.com>
2023-07-02 14:59:36 +0200

    organized js folders

---
+++ src/js/pagesJs/products.js
@@ -0,0 +1,5 @@
+import {controller} from "../MVC patern/controller.js";
+import {view} from "../MVC patern/view.js";
+import {model}  from "../MVC patern/model.js";
+
+controller(model, view).products();
--- src/js/product.js
+++
@@ -1,5 +0,0 @@
-import {controller} from "./controller.js";
-import {view} from "./view.js";
-import {model}  from "./model.js";
-
-controller(model, view).product();
